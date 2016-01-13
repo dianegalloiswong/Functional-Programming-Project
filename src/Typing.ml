aa3c0d74c340d15c82d0c225660653b66c314363
dianegalloiswong <diane.gallois-wong@ens.fr>
2016-01-13 14:49:42 +0100

    Task 1: type-checking

--- src/Typing.ml
+++ src/Typing.ml
@@ -11,7 +11,18 @@ let type_error msg = raise (Type_error msg)
 
 (* Subtyping check *)
 
-let rec subtype t1 t2 = false 
+let rec subtype t1 t2 = match t1, t2 with
+  | _, Top -> true
+  | _ when t1 = t2 -> true
+  | Int, Float -> true
+  | Arrow (u1, v1), Arrow (u2, v2) -> (subtype v1 v2) && (subtype u2 u1)
+  | Record l1, Record l2 ->
+      List.for_all (fun (lab, t) ->
+	    try subtype (List.assoc lab l1) t
+		with Not_found -> false
+	  ) l2
+  | _ -> false
+	  
 
 (* Infer the principal type for expression [e] in typing environment [env].
    Raise [Type_error] if the expression is ill-typed. *)
@@ -24,6 +35,20 @@ let rec infer env e =
       | Some t -> t
       | None -> type_error (sprintf "unbound variable %s" v)
       end
+  | Eabstr (x, tx, e) ->
+      let env1 = add_typenv x tx env in
+	  let te = infer env1 e in
+	  Arrow (tx, te)
+  | Eapp (e1, e2) ->
+      let targ, tres = match infer env e1 with
+	    | Arrow (targ, tres) -> targ, tres
+		| t1 -> type_error (sprintf "expected an arrow type, got %s" (pretty_typ t1))
+	  in
+	  check env e2 targ;
+	  tres
+  | Elet (x, e1, e2) ->
+      let t1 = infer env e1 in
+	  infer (add_typenv x t1 env) e2
   (* Arithmetic *)
   | Econst c ->
       type_of_constant c
@@ -37,10 +62,17 @@ let rec infer env e =
       check env e2 targ2;
       tres
   (* Records *)
+  | Erecord l -> Record (List.map (fun (lab, e) -> (lab, infer env e)) l)
+      (* TODO: check that labels are all distinct? *)
+  | Efield (e, lab) ->
+      let t = infer env e in
+	  let rt = match t with Record rt -> rt | _ -> type_error "expected a record type" in
+	  (try List.assoc lab rt
+	  with Not_found -> type_error ("unknown label "^lab))
   (* Type constraint *)
   | Econstraint(e, t) ->
       check env e t; t
-  | _ -> type_error "TODO" 
+
 
 (* Check that expression [e] has type [t] in typing environment [env].
    Return [()] if true.  Raise [Type_error] if not. *)
